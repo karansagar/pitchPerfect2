06a3570e61bc16e38e42b8356de16280f5e9d2fa
Karan Sagar <karan@karans-macbook-air.local>
2018-03-09 16:42:32 +0530

    added multiple Functionality of sounds (6) . wrapped codes and distributed equally !

--- pitchPerfect2/PlaySoundsViewController.swift
+++ pitchPerfect2/PlaySoundsViewController.swift
@@ -7,21 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
     var recordedAudioURL:URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode:AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType:Int{case slow = 0, fast, chipmunk = 2, vader = 3, echo = 4, reverb = 5}
+
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch (ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 5.5)
+        case .chipmunk:
+            playSound(rate: 1000)
+        case .vader:
+            playSound(rate: -1000)
+        case .echo:
+            playSound(echo:true)
+        case .reverb:
+            playSound(reverb:true)
+        }
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+        stopAudio()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupAudio()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
     
 
